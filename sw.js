5eb7dc97bb93b1980143c5a0a5501bbbcd6195ec
Yuksel Kilinc <yukselkilinc889@gmail.com>
2026-07-10 17:19:06 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'gre-vocab';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './database.js',
+  './manifest.json',
+  './icon.svg',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
